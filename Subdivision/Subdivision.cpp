a23c8010be01b8dd7409e237f28a56bc1faef967
Csaba Hermann <hcsabi98@gmail.com>
2021-10-03 16:35:23 +0200

    Imported OpenGL project
    Create and move points

--- Subdivision/Subdivision.cpp
+++ Subdivision/Subdivision.cpp
@@ -1,20 +1,352 @@
-// Subdivision.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
+#include "glm/glm.hpp"
+
+#include <array>
+#include <vector>
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <math.h>
 
-int main()
+using namespace std;
+
+static int WIN_WIDTH = 400;
+static int WIN_HEIGHT = 400;
+
+GLint dragged = -1;
+
+GLfloat xNorm;
+GLfloat yNorm;
+
+static std::vector<glm::vec3> myPoints =
 {
-    std::cout << "Hello World!\n";
+	glm::vec3(-0.5f, -0.5f, 0.0f),
+	glm::vec3(0.5f, 0.5f, 0.0f),
+};
+
+
+/* Vertex buffer objektum és vertex array objektum az adattároláshoz.*/
+GLuint VBO;
+GLuint VAO;
+
+GLuint renderingProgram;
+
+bool checkOpenGLError() {
+	bool foundError = false;
+	int glErr = glGetError();
+	while (glErr != GL_NO_ERROR) {
+		cout << "glError: " << glErr << endl;
+		foundError = true;
+		glErr = glGetError();
+	}
+	return foundError;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void printShaderLog(GLuint shader) {
+	int len = 0;
+	int chWrittn = 0;
+	char* log;
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+	if (len > 0) {
+		log = (char*)malloc(len);
+		glGetShaderInfoLog(shader, len, &chWrittn, log);
+		cout << "Shader Info Log: " << log << endl;
+		free(log);
+	}
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void printProgramLog(int prog) {
+	int len = 0;
+	int chWrittn = 0;
+	char* log;
+	glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &len);
+	if (len > 0) {
+		log = (char*)malloc(len);
+		glGetProgramInfoLog(prog, len, &chWrittn, log);
+		cout << "Program Info Log: " << log << endl;
+		free(log);
+	}
+}
+
+string readShaderSource(const char* filePath) {
+	string content;
+	ifstream fileStream(filePath, ios::in);
+	string line = "";
+
+	while (!fileStream.eof()) {
+		getline(fileStream, line);
+		content.append(line + "\n");
+	}
+	fileStream.close();
+	return content;
+}
+
+GLuint createShaderProgram() {
+
+	GLint vertCompiled;
+	GLint fragCompiled;
+	GLint linked;
+
+	string vertShaderStr = readShaderSource("vertexShader.glsl");
+	string fragShaderStr = readShaderSource("fragmentShader.glsl");
+
+	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	const char* vertShaderSrc = vertShaderStr.c_str();
+	const char* fragShaderSrc = fragShaderStr.c_str();
+
+	glShaderSource(vShader, 1, &vertShaderSrc, NULL);
+	glShaderSource(fShader, 1, &fragShaderSrc, NULL);
+
+	glCompileShader(vShader);
+	checkOpenGLError();
+	glGetShaderiv(vShader, GL_COMPILE_STATUS, &vertCompiled);
+	if (vertCompiled != 1) {
+		cout << "vertex compilation failed" << endl;
+		printShaderLog(vShader);
+	}
+
+
+	glCompileShader(fShader);
+	checkOpenGLError();
+	glGetShaderiv(vShader, GL_COMPILE_STATUS, &fragCompiled);
+	if (fragCompiled != 1) {
+		cout << "fragment compilation failed" << endl;
+		printShaderLog(fShader);
+	}
+
+	// Shader program objektum létrehozása. Eltároljuk az ID értéket.
+	GLuint vfProgram = glCreateProgram();
+	glAttachShader(vfProgram, vShader);
+	glAttachShader(vfProgram, fShader);
+
+	glLinkProgram(vfProgram);
+	checkOpenGLError();
+	glGetProgramiv(vfProgram, GL_LINK_STATUS, &linked);
+	if (linked != 1) {
+		cout << "linking failed" << endl;
+		printProgramLog(vfProgram);
+	}
+
+	glDeleteShader(vShader);
+	glDeleteShader(fShader);
+
+	return vfProgram;
+}
+
+GLfloat dist2(glm::vec3 P1, glm::vec3 P2)
+{
+	GLfloat t1 = P1.x - P2.x;
+	GLfloat t2 = P1.y - P2.y;
+
+	return t1 * t1 + t2 * t2;
+}
+
+GLint getActivePoint(vector<glm::vec3> p, GLint size, GLfloat sens, GLfloat x, GLfloat y)
+{
+
+	GLint i;
+	GLfloat s = sens * sens;
+
+	xNorm = x / (WIN_WIDTH / 2) - 1.0f;
+	yNorm = y / (WIN_HEIGHT / 2) - 1.0f;
+	glm::vec3 P = glm::vec3(xNorm, yNorm, 0.0f);
+
+	for (i = 0; i < size; i++) {
+		if (dist2(p[i], P) < s) {
+			return i;
+		}
+	}
+	return -1;
+
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+
+}
+
+void updateData()
+{
+	std::vector<glm::vec3> allPoints;
+
+	allPoints.insert(std::end(allPoints), std::begin(myPoints), std::end(myPoints));
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, allPoints.size() * sizeof(glm::vec3), allPoints.data(), GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void cursorPosCallback(GLFWwindow* window, double xPos, double yPos)
+{
+	GLint i;
+
+	xNorm = xPos / (WIN_WIDTH / 2) - 1.0f;
+	yNorm = (WIN_HEIGHT - yPos) / (WIN_HEIGHT / 2) - 1.0f;
+
+	if (dragged >= 0)
+	{
+		myPoints.at(dragged).x = xNorm;
+		myPoints.at(dragged).y = yNorm;
+
+		updateData();
+	}
+
+}
+
+void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
+{
+	GLint i;
+	double x, y;
+	glfwGetCursorPos(window, &x, &y);
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+		if ((i = getActivePoint(myPoints, myPoints.size(), 0.1f, x, WIN_HEIGHT - y)) != -1)
+		{
+			dragged = i;
+		}
+	}
+
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
+		dragged = -1;
+	}
+
+	if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_RELEASE)
+	{
+		if ((i = getActivePoint(myPoints, myPoints.size(), 0.1f, x, WIN_HEIGHT - y)) != -1)
+		{
+			myPoints.erase(myPoints.begin() + i);
+		}
+		else
+		{
+			myPoints.push_back(glm::vec3(xNorm, yNorm, 0.0f));
+		}
+
+		updateData();
+	}
+}
+
+void init(GLFWwindow* window) {
+	renderingProgram = createShaderProgram();
+
+	/* Létrehozzuk a szükséges Vertex buffer és vertex array objektumot. */
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+
+	/* Típus meghatározása: a GL_ARRAY_BUFFER nevesített csatolóponthoz kapcsoljuk a buffert (ide kerülnek a vertex adatok). */
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	/* Másoljuk az adatokat a pufferbe! Megadjuk az aktuálisan csatolt puffert,  azt hogy hány bájt adatot másolunk,
+	a másolandó adatot, majd a feldolgozás módját is meghatározzuk: most az adat nem változik a feltöltés után */
+	glBufferData(GL_ARRAY_BUFFER, myPoints.size() * sizeof(glm::vec3), myPoints.data(), GL_STATIC_DRAW);
+
+	/* A puffer kész, lecsatoljuk, már nem szeretnénk módosítani. */
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	/* Csatoljuk a vertex array objektumunkat a konfiguráláshoz. */
+	glBindVertexArray(VAO);
+
+	/* Vertex buffer objektum újracsatolása. */
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	/* Ezen adatok szolgálják a 0 indexû vertex attribútumot (itt: pozíció).
+	Elsõként megadjuk ezt az azonosítószámot.
+	Utána az attribútum méretét (vec3, láttuk a shaderben).
+	Harmadik az adat típusa.
+	Negyedik az adat normalizálása, ez maradhat FALSE jelen példában.
+	Az attribútum értékek hogyan következnek egymás után? Milyen lépésköz után találom a következõ vertex adatait?
+	Végül megadom azt, hogy honnan kezdõdnek az értékek a pufferben. Most rögtön, a legelejétõl veszem õket.*/
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+	/* Engedélyezzük az imént definiált 0 indexû attribútumot. */
+	glEnableVertexAttribArray(0);
+
+	/* Leválasztjuk a vertex array objektumot és a puufert is.*/
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+/** A jelenetünk utáni takarítás. */
+void cleanUpScene()
+{
+	/** Töröljük a vertex puffer és vertex array objektumokat. */
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
+	/** Töröljük a shader programot. */
+	glDeleteProgram(renderingProgram);
+}
+
+void display(GLFWwindow* window, double currentTime) {
+	unsigned int colorLoc;
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // fontos lehet minden egyes alkalommal törölni!
+
+	// aktiváljuk a shader-program objektumunkat.
+	glUseProgram(renderingProgram);
+
+	/*Csatoljuk a vertex array objektumunkat. */
+	glBindVertexArray(VAO);
+
+	colorLoc = glGetUniformLocation(renderingProgram, "color");
+	glUniform3f(colorLoc, 1.0f, 0.0f, 0.0f);
+	glPointSize(10.0f);
+	glDrawArrays(GL_POINTS, 0, myPoints.size());
+	glBindVertexArray(0);
+
+	glBindVertexArray(VAO);
+	colorLoc = glGetUniformLocation(renderingProgram, "color");
+	glUniform3f(colorLoc, 0.0f, 0.0f, 1.0f);
+	glLineWidth(4.0f);
+	glDrawArrays(GL_LINE_STRIP, 0, myPoints.size());
+
+	/* Leválasztjuk, nehogy bármilyen érték felülíródjon.*/
+	glBindVertexArray(0);
+}
+
+int main(void) {
+
+	/* Próbáljuk meg inicializálni a GLFW-t! */
+	if (!glfwInit()) { exit(EXIT_FAILURE); }
+
+	/* A kívánt OpenGL verzió (4.3) */
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	/* Próbáljuk meg létrehozni az ablakunkat. */
+	GLFWwindow* window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "Drag and drop", NULL, NULL);
+
+	/* Válasszuk ki az ablakunk OpenGL kontextusát, hogy használhassuk. */
+	glfwMakeContextCurrent(window);
+
+	glfwSetKeyCallback(window, keyCallback);
+	glfwSetCursorPosCallback(window, cursorPosCallback);
+	glfwSetMouseButtonCallback(window, mouseButtonCallback);
+
+	/* Incializáljuk a GLEW-t, hogy elérhetõvé váljanak az OpenGL függvények. */
+	if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
+	glfwSwapInterval(1);
+
+	/* Az alkalmazáshoz kapcsolódó elõkészítõ lépések, pl. hozd létre a shader objektumokat. */
+	init(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		/* a kód, amellyel rajzolni tudunk a GLFWwindow ojektumunkba. */
+		display(window, glfwGetTime());
+		/* double buffered */
+		glfwSwapBuffers(window);
+		/* események kezelése az ablakunkkal kapcsolatban, pl. gombnyomás */
+		glfwPollEvents();
+	}
+
+	/* töröljük a GLFW ablakot. */
+	glfwDestroyWindow(window);
+	/* Leállítjuk a GLFW-t */
+
+	cleanUpScene();
+
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+}
