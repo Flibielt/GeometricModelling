58af862bb92cf9204df16d7f06300a29f7fc4d34
Csaba Hermann <hcsabi98@gmail.com>
2021-10-17 10:07:43 +0200

    Enabled mouse movement

--- SurfaceSubdivision/SurfaceSubdivision.cpp
+++ SurfaceSubdivision/SurfaceSubdivision.cpp
@@ -44,6 +44,12 @@ glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)WIN_WIDTH / 
 glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
+bool firstMouse = true;
+bool enableMouse = true;
+float yaw   = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch =  0.0f;
+float lastX =  800.0f / 2.0;
+float lastY =  600.0 / 2.0;
 
 std::vector<glm::vec3> points;
 std::vector<SubdivisionSurface> surfaces;
@@ -120,6 +126,8 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
         cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	if (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS)
+		enableMouse = !enableMouse;
 }
 
 /** Kiszámoljuk a kamera mátrixokat. */
@@ -144,6 +152,41 @@ void cursorPosCallback(GLFWwindow* window, double xPos, double yPos)
 
 		updateData();
 	}
+
+	// Mouse movement
+	if (enableMouse == false)
+		return;
+	
+	if (firstMouse)
+    {
+        lastX = xPos;
+        lastY = yPos;
+        firstMouse = false;
+    }
+
+    float xoffset = xPos - lastX;
+    float yoffset = lastY - yPos; // reversed since y-coordinates go from bottom to top
+    lastX = xPos;
+    lastY = yPos;
+
+    float sensitivity = 0.03f; // change this value to your liking
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    // make sure that when pitch is out of bounds, screen doesn't get flipped
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(front);
 
 }
 
