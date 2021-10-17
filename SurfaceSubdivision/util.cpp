4b9792c9cd6bac04882f866dabb2740b25da8b4f
Csaba Hermann <hcsabi98@gmail.com>
2021-10-17 21:41:33 +0200

    Bug fix: Global variables
    Extern in header file and assign value in source file

--- SurfaceSubdivision/util.cpp
+++ SurfaceSubdivision/util.cpp
@@ -1,5 +1,16 @@
 #include "util.hpp"
 
+glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
+bool firstMouse = true;
+bool enableMouse = true;
+float yaw   = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch =  0.0f;
+float lastX =  800.0f / 2.0;
+float lastY =  600.0 / 2.0;
+float deltaTime = 0.0f, lastTime = 0.0f;
+
 bool checkOpenGLError() {
 	bool foundError = false;
 	int glErr = glGetError();
--- SurfaceSubdivision/util.hpp
+++ SurfaceSubdivision/util.hpp
@@ -13,16 +13,11 @@ static int WIN_WIDTH = 800;
 static int WIN_HEIGHT = 800;
 
 // camera
-glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
-bool firstMouse = true;
-bool enableMouse = true;
-float yaw   = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
-float pitch =  0.0f;
-float lastX =  800.0f / 2.0;
-float lastY =  600.0 / 2.0;
-float deltaTime = 0.0f, lastTime = 0.0f;
+extern glm::vec3 cameraPos;
+extern glm::vec3 cameraFront;
+extern glm::vec3 cameraUp;
+extern bool firstMouse, enableMouse;
+extern float yaw, pitch, lastX, lastY, deltaTime, lastTime;
 
 bool checkOpenGLError();
 void printShaderLog(GLuint shader);
