2b9d65de3182f00b181983d8a50218207cd170a4
Csaba Hermann <hcsabi98@gmail.com>
2021-11-06 22:45:53 +0100

    Created new class for modified butterfly

---
+++ SurfaceSubdivision/ModifiedButterflySubdivision.cpp
@@ -0,0 +1,20 @@
+#include "ModifiedButterflySubdivision.hpp"
+
+void ModifiedButterflySubdivision::subdivide()
+{
+    splitEdges();
+    createNewFaces();
+    createEdges();
+}
+
+void ModifiedButterflySubdivision::splitEdges()
+{
+}
+
+void ModifiedButterflySubdivision::createNewFaces()
+{
+}
+
+void ModifiedButterflySubdivision::createEdges()
+{
+}
---
+++ SurfaceSubdivision/ModifiedButterflySubdivision.hpp
@@ -0,0 +1,57 @@
+#ifndef MODIFIED_BUTTERFLY_SUBDIVISION_H
+#define MODIFIED_BUTTERFLY_SUBDIVISION_H
+
+#include <vector>
+
+#include "Vertex.hpp"
+#include "Edge.hpp"
+#include "Face.hpp"
+
+class ModifiedButterflySubdivision
+{
+private:
+    /**
+     * @brief Create new edges.
+     * 
+     * Each edge will be split to two. The new vertex by the edge is splited will be saved to the vertices.
+     * 
+     */
+    void splitEdges();
+
+    /**
+     * @brief Create new faces using the old and new vertices.
+     * 
+     * 
+     * From 1 old face create 4 new faces. 3 using 1 old vertex and 2 new vertices and 1 with the 3 new vertices.
+     * 
+     * The old faces will be deleted.
+     */
+    void createNewFaces();
+
+    /**
+     * @brief Create new edges from the new faces.
+     * 
+     * Delete the old edges, then create new edges from the newly created faces.
+     */
+    void createEdges();
+public:
+    std::vector<Vertex> vertices;
+    std::vector<Edge> edges;
+    std::vector<Face> faces;
+
+    ModifiedButterflySubdivision(/* args */);
+    ~ModifiedButterflySubdivision();
+
+    void subdivide();
+};
+
+ModifiedButterflySubdivision::ModifiedButterflySubdivision(/* args */)
+{
+}
+
+ModifiedButterflySubdivision::~ModifiedButterflySubdivision()
+{
+}
+
+
+#endif
